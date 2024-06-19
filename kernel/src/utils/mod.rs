9331e796863b9ffa02f6a9202f06c5f980303347
Vasant Karasulli <vkarasulli@suse.de>
2024-06-19 09:37:29 +0200

    utils: Add a StringRingBuffer implementation
    The log buffer is implemented using a ring buffer that contains the
    characters within log entries. This patch introduces a generic
    StringRingBuffer that can be used for safely managing the contents of
    strings within a ring buffer that is suitable for use with the log
    buffer. This separates the ring buffer implementation from the log and
    allows implementation of ring buffer specific unit testing, which is
    included within this patch.
    
    Signed-off-by: Vasant Karasulli <vkarasulli@suse.de>
    Signed-off-by: Roy Hopkins <roy.hopkins@suse.com>

--- kernel/src/utils/mod.rs
+++ kernel/src/utils/mod.rs
@@ -9,11 +9,13 @@ pub mod immut_after_init;
 pub mod memory_region;
 pub mod scoped;
 pub mod tcb_ptr;
+pub mod string_ring_buffer;
 pub mod util;
 pub mod vec;
 
 pub use memory_region::MemoryRegion;
 pub use scoped::{ScopedMut, ScopedRef};
+pub use string_ring_buffer::StringRingBuffer;
 pub use util::{
     align_down, align_up, is_aligned, overlap, page_align_up, page_offset, round_to_pages,
     zero_mem_region,
