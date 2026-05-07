4130eb87e98626cdb9cb825f0575a5e23426bd02
Nicola Ramacciotti <niko.ramak@gmail.com>
2026-05-07 14:17:37 +0200

    scripts/test-in-svsm: Add optional timeout handling
    Integrate the `timeout` check in `test-in-svsm.sh` directly to be able
    to test in the same way as when CI is running.
    
    Add the timeout option `--foreground` to keep `Ctrl-]` as interrupt.
    
    As the script can now fail due to timeout, add handling for that exit
    code (124).
    
    Introduce the `--timeout` parameter to enable timeout handling, which
    is disabled by default, as it can also be useful to test without a
    time limit.
    
    Signed-off-by: Nicola Ramacciotti <niko.ramak@gmail.com>

--- scripts/test-in-svsm.sh
+++ scripts/test-in-svsm.sh
@@ -96,6 +96,8 @@ mkfifo $TEST_DIR/pipe.out
 dd if=/dev/urandom of="$TEST_DIR/svsm_state.raw" bs=512 count=1024
 
 LAUNCH_GUEST_ARGS=""
+TIMEOUT_ENABLED=0
+TIMEOUT_TIME=""
 
 while [[ $# -gt 0 ]]; do
   case $1 in
@@ -113,6 +115,12 @@ while [[ $# -gt 0 ]]; do
       shift
       shift
       ;;
+    --timeout)
+      TIMEOUT_ENABLED=1
+      TIMEOUT_TIME=$2
+      shift
+      shift
+      ;;
     --)
       shift
       break
@@ -124,22 +132,35 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
+if [[ $TIMEOUT_ENABLED -eq 1 ]]; then
+    echo "Running tests with timeout: $TIMEOUT_TIME"
+    TIMEOUT_CMD="timeout --foreground $TIMEOUT_TIME"
+else
+    echo "Running tests without timeout"
+    TIMEOUT_CMD=""
+fi
+
 test_io $TEST_DIR/pipe.in $TEST_DIR/pipe.out $VSOCK_PORT &
 TEST_IO_PID=$!
 
 svsm_exit_code=0
 
-$SCRIPT_DIR/launch_guest.sh --igvm $SCRIPT_DIR/../bin/coconut-test-qemu.igvm \
-    --state "$TEST_DIR/svsm_state.raw" \
-    --vsock "$VSOCK_CID" \
-    --unit-tests $TEST_DIR/pipe \
-    $LAUNCH_GUEST_ARGS "$@" || svsm_exit_code=$?
+$TIMEOUT_CMD \
+    $SCRIPT_DIR/launch_guest.sh \
+        --igvm $SCRIPT_DIR/../bin/coconut-test-qemu.igvm \
+        --state "$TEST_DIR/svsm_state.raw" \
+        --vsock "$VSOCK_CID" \
+        --unit-tests $TEST_DIR/pipe \
+        $LAUNCH_GUEST_ARGS "$@" || svsm_exit_code=$?
 
 # SVSM writes 0x10 to the QEMU exit port when all tests passed.
 # This results in QEMU returning 0x21 ((0x10 << 1) | 1)
 if [[ $svsm_exit_code -eq 0x21 ]]; then
     echo "All tests passed"
     exit_value=0
+elif [[ $svsm_exit_code -eq 124 && $TIMEOUT_ENABLED -eq 1 ]]; then
+    echo "Test Failed: timeout"
+    exit_value=1
 else
     echo "Test Failed with exit code: $svsm_exit_code"
     exit_value=1
