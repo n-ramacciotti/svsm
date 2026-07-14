03443b6c6392d353148340c79ee570be3fb8471d
Nicola Ramacciotti <niko.ramak@gmail.com>
2026-07-14 17:32:25 +0200

    kernel/protocols: Implement OCP list request handler
    Add the new call identifier (`0`) to the OCP handler and implement
    `SVSM_OCP_LIST` following the spec.
    
    If some guest params are incorrect, like buffer alignment or zero bytes
    requested, an appropriate error will be returned.
    
    The buffer len is 4-bytes, so make sure to use only the low 32 bits.
    
    Compute the number of entries required and use `GuestPtr<[T]>` to access
    safely the untrusted guest region.
    
    Once an entry is available, it is possible to copy it directly to the guest
    memory without using temporary heap allocation to store all the entries
    together.
    
    As this call requires SVSM to provide a list of entries, extract them from
    the map with the new utility and use `get_object_details` method from the
    new trait to retrieve object info.
    As this call requires SVSM to provide a list of entries, extract them from
    the object with the `get_object_sources` method from the new trait.
    
    Signed-off-by: Nicola Ramacciotti <niko.ramak@gmail.com>

--- kernel/src/protocols/ocp/requests.rs
+++ kernel/src/protocols/ocp/requests.rs
@@ -11,6 +11,24 @@ use alloc::{sync::Arc, vec::Vec};
 
 use super::api::OcpObjectOperations;
 
+use crate::{
+    address::{Address, PhysAddr},
+    mm::ptguards::PerCPUPageMappingGuard,
+    protocols::{RequestParams, errors::SvsmReqError},
+    types::PAGE_SIZE,
+};
+
+use super::source::{OCP_NAME_LEN, OCP_SOURCE_SIZE, OcpSource};
+
+use core::ffi::CStr;
+
+// OCP protocol services
+const SVSM_OCP_LIST: u32 = 0;
+
+const LOW_32_BITS: u64 = 0xffff_ffff;
+const OCP_BUFFER_MAX_SIZE: usize = PAGE_SIZE;
+const OCP_BUFFER_ALIGNMENT: usize = 8;
+
 static OCP_SOURCES: RWLock<Vec<Arc<dyn OcpObjectOperations>>> = RWLock::new(Vec::new());
 
 pub fn add_ocp_object(object: Arc<dyn OcpObjectOperations>) {
@@ -29,8 +47,115 @@ pub fn get_ocp_object(name: &str) -> Option<Arc<dyn OcpObjectOperations>> {
     None
 }
 
+fn ocp_list_request(params: &mut RequestParams) -> Result<(), SvsmReqError> {
+    let gpa_buffer = PhysAddr::from(params.rdx);
+
+    if !gpa_buffer.is_aligned(OCP_BUFFER_ALIGNMENT) {
+        return Err(SvsmReqError::invalid_address());
+    }
+
+    let gpa_name = PhysAddr::from(params.rcx);
+
+    let buffer_size = (params.r8 & LOW_32_BITS) as usize;
+
+    if buffer_size == 0 || buffer_size > OCP_BUFFER_MAX_SIZE {
+        return Err(SvsmReqError::invalid_parameter());
+    }
+
+    if !buffer_size.is_multiple_of(OCP_SOURCE_SIZE) {
+        return Err(SvsmReqError::invalid_parameter());
+    }
+
+    let num_entries = buffer_size / OCP_SOURCE_SIZE;
+
+    let guard = PerCPUPageMappingGuard::create(
+        gpa_buffer.page_align(),
+        gpa_buffer
+            .checked_add(buffer_size)
+            .ok_or(SvsmReqError::invalid_address())?
+            .page_align_up(),
+        0,
+    )?;
+
+    let entries_ptr = guard.guest_slice::<OcpSource>(gpa_buffer.page_offset(), num_entries)?;
+
+    let (bytes_written, max_buffer) = if gpa_name.is_null() {
+        let mut entries_written = 0;
+
+        let objects = OCP_SOURCES.lock_read();
+        let objects_len = objects.len();
+
+        for entry in objects.iter() {
+            if entries_written >= num_entries {
+                break;
+            }
+
+            if entries_ptr
+                .write(entries_written, entry.get_object_details())
+                .is_err()
+            {
+                break;
+            }
+            entries_written += 1;
+        }
+
+        (
+            entries_written * OCP_SOURCE_SIZE,
+            objects_len * OCP_SOURCE_SIZE,
+        )
+    } else {
+        let mut name_slice = [0u8; OCP_NAME_LEN];
+        let name_guard = PerCPUPageMappingGuard::create(
+            gpa_name.page_align(),
+            gpa_name
+                .checked_add(OCP_NAME_LEN)
+                .ok_or(SvsmReqError::invalid_address())?
+                .page_align_up(),
+            0,
+        )?;
+        let name_ptr = name_guard.guest_slice::<u8>(gpa_name.page_offset(), OCP_NAME_LEN)?;
+        name_ptr.read_to_slice(&mut name_slice)?;
+        let obj_name = CStr::from_bytes_until_nul(&name_slice)
+            .map_err(|_| SvsmReqError::invalid_parameter())?
+            .to_str()
+            .map_err(|_| SvsmReqError::invalid_parameter())?;
+
+        let Some(object) = get_ocp_object(obj_name) else {
+            return Err(SvsmReqError::invalid_parameter());
+        };
+
+        let sources_len = object.get_object_source_count();
+
+        let mut entries_written = 0;
+
+        for entry in object.get_object_sources() {
+            if entries_written >= num_entries {
+                break;
+            }
+
+            if entries_ptr
+                .write(entries_written, entry.get_source_details())
+                .is_err()
+            {
+                break;
+            }
+            entries_written += 1;
+        }
+        (
+            entries_written * OCP_SOURCE_SIZE,
+            sources_len * OCP_SOURCE_SIZE,
+        )
+    };
+
+    params.r8 = bytes_written as u64;
+    params.r9 = max_buffer as u64;
+
+    Ok(())
+}
+
 pub fn ocp_protocol_request(request: u32, params: &mut RequestParams) -> Result<(), SvsmReqError> {
     match request {
+        SVSM_OCP_LIST => ocp_list_request(params),
         _ => Err(SvsmReqError::unsupported_call()),
     }
 }
