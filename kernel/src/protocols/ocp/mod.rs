f1c74e089c7924e04775dc414bcc07451e724372
Nicola Ramacciotti <niko.ramak@gmail.com>
2026-07-28 11:13:15 +0200

    kernel/protocols: Add OCP to core query protocol
    The query protocol is used to check the availability of a protocol at a
    given version.
    
    It should take into account every available protocol. Add the new OCP
    protocol management.
    
    Signed-off-by: Nicola Ramacciotti <niko.ramak@gmail.com>

--- kernel/src/protocols/ocp/mod.rs
+++ kernel/src/protocols/ocp/mod.rs
@@ -10,4 +10,7 @@ pub mod api;
 pub mod requests;
 pub mod source;
 
-pub use requests::{add_ocp_object, ocp_protocol_request};
+pub use requests::{
+    OBSERVABILITY_CONFIGURATION_PROTOCOL_VERSION_MAX,
+    OBSERVABILITY_CONFIGURATION_PROTOCOL_VERSION_MIN, add_ocp_object, ocp_protocol_request,
+};
--- kernel/src/protocols/ocp/requests.rs
+++ kernel/src/protocols/ocp/requests.rs
@@ -31,6 +31,10 @@ const LOW_32_BITS: u64 = 0xffff_ffff;
 const OCP_BUFFER_MAX_SIZE: usize = PAGE_SIZE;
 const OCP_BUFFER_ALIGNMENT: usize = 8;
 
+// Min and Max supported protocol version
+pub const OBSERVABILITY_CONFIGURATION_PROTOCOL_VERSION_MIN: u32 = 1;
+pub const OBSERVABILITY_CONFIGURATION_PROTOCOL_VERSION_MAX: u32 = 1;
+
 static OCP_SOURCES: RWLock<Vec<Arc<dyn OcpObjectOperations>>> = RWLock::new(Vec::new());
 
 pub fn add_ocp_object(object: Arc<dyn OcpObjectOperations>) {
